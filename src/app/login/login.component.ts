82037f344b2633469a84ee52df0537f70a6844db
nibortheman <punkschoolkid@gmail.com>
2021-03-09 21:58:38 +0800

    3 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,23 @@
-import { Component } from "@angular/core";
-import { UserData } from "./userdata";
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { FormGroupState } from 'ngrx-forms';
+import { Observable } from 'rxjs';
+
+import { FormValue, State } from './async-validation.reducer';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.css"]
+  selector: 'ngf-async-validation',
+  templateUrl: './async-validation.component.html',
+  styleUrls: ['./async-validation.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent {
-  userModel = new UserData();
+export class AsyncValidationPageComponent {
+  formState$: Observable<FormGroupState<FormValue>>;
+  searchResults$: Observable<string[]>;
+
+  constructor(store: Store<State>) {
+    this.formState$ = store.pipe(select(s => s.asyncValidation.formState));
+    this.searchResults$ = store.pipe(select(s => s.asyncValidation.searchResults));
+  }
 }
+© 2021 GitHub, Inc.
--- src/app/login/userdata.ts
+++ src/app/login/userdata.ts
@@ -1,3 +1,52 @@
-export class UserData {
-  constructor(public email: string, public password: string) {}
+import { Action, combineReducers } from '@ngrx/store';
+import { createFormGroupState, createFormStateReducerWithUpdate, FormGroupState, updateGroup, validate } from 'ngrx-forms';
+import { greaterThan, required } from 'ngrx-forms/validation';
+
+import { State as RootState } from '../app.reducer';
+
+export interface FormValue {
+  searchTerm: string;
+  numberOfResultsToShow: number;
+}
+
+export interface State extends RootState {
+  asyncValidation: {
+    formState: FormGroupState<FormValue>;
+    searchResults: string[];
+  };
+}
+
+export class SetSearchResultAction implements Action {
+  static readonly TYPE = 'asyncValidation/SET_SEARCH_RESULT';
+  readonly type = SetSearchResultAction.TYPE;
+  constructor(public results: string[]) { }
+}
+
+export const FORM_ID = 'asyncValidation';
+
+export const INITIAL_STATE = createFormGroupState<FormValue>(FORM_ID, {
+  searchTerm: '',
+  numberOfResultsToShow: 5,
+});
+
+const formGroupReducerWithUpdate = createFormStateReducerWithUpdate<FormValue>(updateGroup<FormValue>({
+  searchTerm: validate(required),
+  numberOfResultsToShow: validate(required, greaterThan(0)),
+}));
+
+const reducers = combineReducers<State['asyncValidation']>({
+  formState(s = INITIAL_STATE, a: Action) {
+    return formGroupReducerWithUpdate(s, a);
+  },
+  searchResults(s: string[] = [], a: Action) {
+    if (a.type === SetSearchResultAction.TYPE) {
+      return (a as SetSearchResultAction).results;
+    }
+
+    return s;
+  },
+});
+
+export function reducer(s: State['asyncValidation'], a: Action) {
+  return reducers(s, a);
 }
