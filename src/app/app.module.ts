3e4fdee4ff803c90f8f528f280c6a486479b8a93
nibortheman <punkschoolkid@gmail.com>
2021-03-10 12:22:02 +0800

    3 files modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,22 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { ReactiveFormsModule } from "@angular/forms";
 import { FormsModule } from "@angular/forms";
-
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { LoginComponent } from "./login/login.component";
 import { StoreModule } from "@ngrx/store";
 import { NgrxFormsModule } from "ngrx-forms";
-
 import { reducers } from "./login/login.reducer";
+
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    LoginComponent,
-    NgrxFormsModule,
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+     NgrxFormsModule,
     StoreModule.forRoot(reducers)
   ],
+  declarations: [AppComponent, HelloComponent, LoginComponent],
   providers: [],
   bootstrap: [LoginComponent]
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,24 +1,17 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { select, Store } from "@ngrx/store";
+import { Component } from "@angular/core";
+import { Store } from "@ngrx/store";
 import { FormGroupState } from "ngrx-forms";
-import { Observable } from "rxjs";
-
-import { FormValue, State } from "./login.reducer";
+import { Observable } from "rxjs/Observable";
+import { AppState, MyFormValue } from "./login.reducer";
 
 @Component({
   selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.css"],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  templateUrl: "./login.component.html"
 })
 export class LoginComponent {
-  formState$: Observable<FormGroupState<FormValue>>;
-  searchResults$: Observable<string[]>;
+  formState$: Observable<FormGroupState<MyFormValue>>;
 
-  constructor(store: Store<State>) {
-    this.formState$ = store.pipe(select(s => s.asyncValidation.formState));
-    this.searchResults$ = store.pipe(
-      select(s => s.asyncValidation.searchResults)
-    );
+  constructor(private store: Store<AppState>) {
+    this.formState$ = store.select(s => s.myForm);
   }
 }
